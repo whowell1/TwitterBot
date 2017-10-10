8d029bd51fe32beb89b8f597ed12f115a7284d53
wren <wrentarohowell@gmail.com>
2017-10-10 13:09:10 -0400

    Intitial Commit

---
+++ bot.js
@@ -0,0 +1,55 @@
+console.log("The bot is starting ");
+
+var Twit = require('twit');
+
+var config = require('./config');
+// console.log(config);
+var T = new Twit(config);
+
+var params = {
+  q: 'Donald Trump since:2017-10-4',
+  count: 8,
+  result_type: 'recent',
+  lang: 'en'
+}
+
+
+
+
+var wordsToReplace = ["Trump", "fake news", "donald", "ivanka", "Donald Trump"];
+
+
+var wordsToOverWriteWith = ["OJ Simpson"];
+function gotData(err, data, response){
+  var tweets = data.statuses;
+  for (var i = 0; i < tweets.length; i++){
+  console.log(tweets[i].text.replace(wordsToReplace , wordsToOverWriteWith));
+  }
+}
+ var listOfTweets = T.get('search/tweets', params,gotData);
+
+
+
+
+
+
+
+
+
+
+
+// var tweet = {
+//
+//   status: '#This is testbot wow. '
+// }
+
+// T.post('statuses/update' , tweet, tweeted);
+//
+//
+// function tweeted(err, data,response) {
+//   if (err){
+//     console.log("Something went wrong");
+//   } else {
+//     console.log("It worked");
+//   }
+// }
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports =  {
+  consumer_key: 'DLiLpvylEngKUhPRCNttib5BT'
+, consumer_secret: 'HpP4rgb1RR8aiWsJsFiH01p0mpR9cm8ssKWEr5CtiPQyBcTpdd'
+, access_token: '913833983607283712-yhUGGklCrH8eFBnnavpms4nKE00BsIU'
+, access_token_secret: 'Mqpxyyj3aZAs3U4ORexJ0g6IfUYAvSEzL7pqJaYM0HWav'
+
+}
