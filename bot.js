82f48e70ac5f66a9a67c2403208b5f0d41613f95
wren <wrentarohowell@gmail.com>
2017-10-12 18:42:05 -0400

    updated code

--- bot.js
+++ bot.js
@@ -13,33 +13,54 @@ var params = {
   lang: 'en'
 }
 
+var wordsToReplace = ['Trump', 'fake news', 'Donald', 'Ivanka'];
 
-var wordsToReplace = ["Trump"];
-
-
-var wordsToOverWriteWith = ["OJ Simpson"];
-function gotData(err, data, response){
+function gotData(err, data, response) {
   var tweets = data.statuses;
-  for (var i = 0; i < tweets.length; i++){
-      console.log(tweets[i].text.replace(wordsToReplace , wordsToOverWriteWith));
+  var modifiedTweets = [];
+  for (var i = 0; i < tweets.length; i++) {
+    // console.log(tweets[i].text.replace(wordsToReplace , wordsToOverWriteWith));
+    modifiedTweet = containsWordsToReplace(tweets[i].text, wordsToReplace);
+    modifiedTweets.push(modifiedTweet);
+
+  }
+  
+   var tweet = {
+    status: modifiedTweets[0]
+    }
+
+  T.post('statuses/update' , tweet, tweeted);
+}
+
+
+function containsWordsToReplace(text, replacements) {
+  var split = text.split(" ");
+  var modifiedTweet = split.map(function(word) {
+    for (var i = 0; i < replacements.length; i++) {
+      if (word.includes(replacements[i])) {
+        word = '************';
+      }
+    }
+    return word;
+  });
+  return modifiedTweet.join(" ");
+}
+var listOfTweets = T.get('search/tweets', params, gotData);
+
+
+
+
+
+
+// console.log(gotData());
+
+
+
+
+function tweeted(err, data,response) {
+  if (err){
+    console.log("Something went wrong");
+  } else {
+    console.log("It worked");
   }
 }
- var listOfTweets = T.get('search/tweets', params,gotData);
-
-
-
-// var tweet = {
-//
-//   status: '#This is testbot wow. '
-// }
-//
-// T.post('statuses/update' , tweet, tweeted);
-//
-//
-// function tweeted(err, data,response) {
-//   if (err){
-//     console.log("Something went wrong");
-//   } else {
-//     console.log("It worked");
-//   }
-// }
