43f055b5af5d4ff7076298f97ac0ac843dd1aba8
wren <wrentarohowell@gmail.com>
2017-10-15 23:31:55 -0400

    Added link

--- bot.js
+++ bot.js
@@ -4,12 +4,7 @@ var Twit = require('twit');
 var catNames = require('cat-names');
 var config = require('./config');
 var debug = true;
-
-var GoogleBooksKey = 'AIzaSyBvAHOQSUGks4yvyrfDIEjv7letuP9mHpo';
-
 var T = new Twit(config);
-
-
 var names = ["Khorloogiin Choibalsan", "Adolf Hitler", "Francisco Franco", "Idi Amin",
   "Joseph Stalin", "Bashar al-Assad",
   "Christoper Columbus", "King Leopold II", "Augusto Pinochet",
@@ -17,12 +12,9 @@ var names = ["Khorloogiin Choibalsan", "Adolf Hitler", "Francisco Franco", "Idi 
   "Chiang Kai-shek"
 ];
 
-
 var issues = ["global warming", "education", "poverty", "womens rights", "gay rights",
   "civil war", " ethnic violence", "corrupt police", "cyber warfare"
 ];
-
-
 
 Array.prototype.pick = function() {
   return this[Math.floor(Math.random() * this.length)];
@@ -35,44 +27,46 @@ function history() {
   var facts = "";
   var url = ""
   names = names.pickAndPad();
-  // if (names == " Idi Amin") {
-  //   url = "https://goo.gl/YaXphN";
-  // } else if (names == "Khorloogiin Choibalsan") {
-  //   url = "https://goo.gl/Azeq8s";
-  // } else if (names == "Adolf Hitler") {
-  //   url = "https://goo.gl/qU3Y81";
-  // } else if (names == "Francisco Franco") {
-  //   url = "https://goo.gl/ETjQjB";
-  // } else if (names == "King Leopold II") {
-  //   url = "https://goo.gl/sSsjGG";
-  // } else if (names == "Bashar al-Assad") {
-  //   url = "https://goo.gl/i1xk9B";
-  // } else if (names == "Augusto Pinochet") {
-  //   url = "https://goo.gl/USfiYA";
-  // } else if (names == "Talaat Pasha") {
-  //   url = "https://goo.gl/E9jA8s";
-  // } else if (names == "Tomás de Torquemada") {
-  //   url = "https://goo.gl/cF2WcV";
-  // } else if (names == "Robert Mugabe") {
-  //   url = "https://goo.gl/KjTajt";
-  // } else if (names == "Kōki Hirota") {
-  //   url = "https://goo.gl/mdTFpT";
-  // } else if (names == "Chiang Kai-shek") {
-  //   url = "https://goo.gl/7BMY7R";
-  // } else {
-  //   url = "";
-  // }
+  if (names.includes("Idi Amin")) {
+    url = "https://goo.gl/YaXphN";
+  } else if (names.includes("Khorloogiin Choibalsan")) {
+    url = "https://goo.gl/Azeq8s";
+  } else if (names.includes("Adolf Hitler")) {
+    url = "https://goo.gl/qU3Y81";
+  } else if (names.includes("Francisco Franco")) {
+    url = "https://goo.gl/ETjQjB";
+  } else if (names.includes("King Leopold II")) {
+    url = "https://goo.gl/sSsjGG";
+  } else if (names.includes("Bashar al-Assad")) {
+    url = "https://goo.gl/i1xk9B";
+  } else if (names.includes("Augusto Pinochet")) {
+    url = "https://goo.gl/USfiYA";
+  } else if (names.includes("Talaat Pasha")) {
+    url = "https://goo.gl/E9jA8s";
+  } else if (names.includes("Tomas de Torquemada")) {
+    url = "https://goo.gl/cF2WcV";
+  } else if (names.includes("Robert Mugabe")) {
+    url = "https://goo.gl/KjTajt";
+  } else if (names.includes("Koki Hirota")) {
+    url = "https://goo.gl/mdTFpT";
+  } else if (names.includes("Chiang Kai-shek")) {
+    url = "https://goo.gl/7BMY7R";
+  } else if(names.includes("Christoper Columbus")) {
+    url = "https://goo.gl/MEH8g6";
+  }
+  else{
+    url = '';
+  }
     facts += names
     facts += "killed more than "
     facts += " " + Math.floor(Math.random() * 10000)
     facts += " people "
     facts += issues.pickAndPad();
-    facts += "ruins more lives. Read about " + names + "on Wiki.";
+    facts += "ruins more lives. Read about " + names + "on Wiki ";
     facts += url;
-
-    console.log(facts.trim());
-    console.log(facts.trim().length);
-    // return facts.trim();
+    // console.log(facts);
+    // console.log(facts.trim());
+    return facts;
   }
 
 
